7b36dfd198c5a31c41c610ece1d6da1a0ed0c651
Andy Cary <info@andycary.com>
2022-02-18 22:09:29 -0500

    working but not optimized for denominations

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -6,6 +6,7 @@ public class CashRegister {
 	
 	static double itemPrice = 0;
 	static double amountTendered = 0;
+	static double newDiff = 0;
 
 	public static void main(String[] args) {
 		
@@ -13,33 +14,150 @@ public class CashRegister {
 
 		getItemPrice(kb);
 		getAmountTendered(kb);
-		comparePriceToTendered();
-		makeChange();
 		
 	}
 	
 	public static void getItemPrice(Scanner kb) {
 		System.out.println("Provide item price:");
 		itemPrice = kb.nextDouble();
+		System.out.println();
 	}
 	
 	public static void getAmountTendered(Scanner kb) {
 		System.out.println("Enter amount tendered:");
 		amountTendered = kb.nextDouble();
+		System.out.println();
+		comparePriceToTendered();
 	}
 	
 	public static void comparePriceToTendered() {
-		System.out.println("Checking...");
 		
-		// TODO: Compare price to tendered
+		if (itemPrice == amountTendered) {
+			System.out.println("Exact amount.");
+		}
+		else if (amountTendered < itemPrice) {
+			System.out.println("Amount tendered is less than the purchase price.");
+			System.out.println("Please redo transaction.");
+			System.exit(0);
+		}
+		else if (amountTendered > itemPrice ) {
+			double difference = amountTendered - itemPrice;
+			makeChange(difference);
+		}
+		
+		System.out.println();
 	}
 	
-	public static void makeChange() {
-		System.out.println("Making change...");
+	public static void makeChange(double difference) {
+		System.out.println("Making change for: " + difference);
 		
-		// TODO: Make change
+		// TODO: Make change ($20 ,$10 ,$5 ,$1, .25c, .10c, .05c, .01c)
+		double twenty =  0; // $20
+		double ten = 0; // $10
+		double five = 0; // $5
+		double one = 0; // $1
+		double quarter = 0; // $0.25
+		double dime = 0; // $0.10
+		double nickel = 0; // $0.05
+		double penny = 0; // $0.01
 		
-		System.out.println("Done.");
+		double remaining = difference;
+		System.out.println("Remaining: " + remaining);
+		
+		while (remaining > 0.0) {
+			if (remaining % 20 == 0.0) {
+				if (remaining > 0.0) {
+					twenty = twenty + 1;
+					twenty *= twenty / 20;
+					remaining = remaining - (twenty * 20.0);
+					System.out.println("New remainder: " + remaining);
+				}
+			}
+			if (remaining % 10 == 0.0) {
+				if (remaining > 0.0) {
+					ten = ten + 1;
+					ten *= ten / 10;
+					remaining = remaining - (ten * 10.0);
+					System.out.println("New remainder: " + remaining);
+				}
+			}
+			if (remaining % 5 == 0.0) {
+				if (remaining > 0.0) {
+					five = five + 1;
+					five *= five / 5;
+					remaining = remaining - (five * 5.0);
+					System.out.println("New remainder: " + remaining);
+				}
+			}
+			if (remaining % 1 == 0.0) {
+				if (remaining > 0.0) {
+					one = one + 1;
+					one *= remaining / 1;
+					remaining = remaining - (one * 1.0);
+					System.out.println("New remainder: " + remaining);
+				}
+			}
+			if (remaining % 0.25 == 0.0) {
+				if (remaining > 0.0) {
+					quarter = quarter + 1;
+					quarter *= quarter / 0.25;
+					remaining = remaining - (quarter * 0.25);
+					System.out.println("New remainder: " + remaining);
+				}
+			}
+			if (remaining % 0.10 == 0.0) {
+				if (remaining > 0.0) {
+					dime = dime + 1;
+					dime *= dime / 0.10;
+					remaining = remaining - (dime * 0.10);
+					System.out.println("New remainder: " + remaining);
+				}
+			}
+			if (remaining % 0.05 == 0.0) {
+				if (remaining > 0.0) {
+					nickel = nickel + 1;
+					nickel *= nickel / 0.05;
+					remaining = remaining - (nickel * 0.05);
+					System.out.println("New remainder: " + remaining);
+				}
+			}
+			if (remaining % 0.01 == 0.0) {
+				if (remaining > 0.0) {
+					penny = penny + 1;
+					penny *= penny / 0.01;
+					remaining = remaining - (penny * 0.01);
+					System.out.println("New remainder: " + remaining);
+				}
+			}
+		}
+		
+		System.out.println();
+		System.out.println("-------- CHANGE --------");
+		if (twenty != 0) {
+			System.out.println("Twenty: " + twenty);
+		}
+		if (ten != 0) {
+			System.out.println("Ten: " + ten);
+		}
+		if (five != 0) {
+			System.out.println("Five: " + five);
+		}
+		if (one != 0) {
+			System.out.println("One: " + one);
+		}
+		if (quarter != 0) {
+			System.out.println("Quarter: " + quarter);
+		}
+		if (dime != 0) {
+			System.out.println("Dime: " + dime);
+		}
+		if (nickel != 0) {
+			System.out.println("Nickel: " + nickel);
+		}
+		if (penny != 0) {
+			System.out.println("Penny: " + penny);
+		}
+		System.out.println("-------- DONE --------");
 	}
 
 }
